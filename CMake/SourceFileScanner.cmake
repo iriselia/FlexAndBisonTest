7d2edd8be6841384fe95397a0576c89f17f1965a
Frank Park <ypiao@wpi.edu>
2014-12-07 14:00:25 -0500

    Initial Commit

--- CMake/SourceFileScanner.cmake
+++ CMake/SourceFileScanner.cmake
@@ -105,6 +105,10 @@ MACRO(ADD_MSVC_PRECOMPILED_HEADER PrecompiledHeader PrecompiledSource SourcesVar
   ENDIF(MSVC)
 ENDMACRO(ADD_MSVC_PRECOMPILED_HEADER)
 
+#
+#
+#
+#
 function(REMOVE_FILE_EXTENSION inFiles outFiles)
 	foreach(currFile ${inFiles})
 		GET_FILENAME_COMPONENT(filePath ${currFile} PATH)
@@ -132,16 +136,19 @@ function(setup_solution SOLUTION_NAME)
 		
 		# Pre-Pre-Configure Processing
 		# Source file update and generation (flex and bison, github)
-		SET( FLEX_EXECUTABLE "${CMAKE_SOURCE_DIR}/CMake/flex_bison/flex.exe")
-		SET( BISON_EXECUTABLE "${CMAKE_SOURCE_DIR}/CMake/flex_bison/bison.exe")
-		file(GLOB_RECURSE MY_FLEX_FILES ${fileDir}/*.l)
-		file(GLOB_RECURSE MY_BISON_FILES ${fileDir}/*.y)
-		REMOVE_FILE_EXTENSION(${MY_FLEX_FILES} flexFiles)
-		REMOVE_FILE_EXTENSION(${MY_BISON_FILES} bisonFiles)
-		SET( FlexArgs "-o${flexFiles}.flex.c" "${flexFiles}.l")
-		SET( BisonArgs "-o${bisonFiles}.bison.c" "-d" "${bisonFiles}.y" )
-		execute_process(COMMAND ${FLEX_EXECUTABLE} ${FlexArgs})
-		execute_process(COMMAND ${BISON_EXECUTABLE} ${BisonArgs})
+		if( PRECOMPILE_FLEX_AND_BISON AND NOT FLEX_EXECUTABLE STREQUAL "" AND NOT BISON_EXECUTABLE STREQUAL "")
+			file(GLOB_RECURSE MY_FLEX_FILES ${fileDir}/*.l)
+			file(GLOB_RECURSE MY_BISON_FILES ${fileDir}/*.y)
+			
+			if(MY_FLEX_FILES AND MY_BISON_FILES)
+				REMOVE_FILE_EXTENSION(${MY_FLEX_FILES} flexFiles)
+				REMOVE_FILE_EXTENSION(${MY_BISON_FILES} bisonFiles)
+				SET( FlexArgs "-o${flexFiles}.flex.c" "${flexFiles}.l")
+				SET( BisonArgs "-o${bisonFiles}.bison.c" "-d" "${bisonFiles}.y" )
+				execute_process(COMMAND ${FLEX_EXECUTABLE} ${FlexArgs})
+				execute_process(COMMAND ${BISON_EXECUTABLE} ${BisonArgs})
+			endif()
+		endif()
 		
 		#parse the directory name for caching project specific include dirs
 		string(REPLACE "/" ";" p2list "${fileDir}")
@@ -216,11 +223,13 @@ endif()
 file(GLOB_RECURSE MY_PRECOMPILED_HEADER ${CMAKE_CURRENT_SOURCE_DIR}/stdafx.h)
 file(GLOB_RECURSE MY_PRECOMPILED_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/stdafx.cpp)
 #----- Flex and Bison -----
-file(GLOB_RECURSE MY_FLEX_FILES ${CMAKE_CURRENT_SOURCE_DIR}/*.l)
-file(GLOB_RECURSE MY_BISON_FILES ${CMAKE_CURRENT_SOURCE_DIR}/*.y)
-# remove extension from .l and .y files
-REMOVE_FILE_EXTENSION(${MY_FLEX_FILES} flexFiles)
-REMOVE_FILE_EXTENSION(${MY_BISON_FILES} bisonFiles)
+if( USE_FLEX_AND_BISON )
+	file(GLOB_RECURSE MY_FLEX_FILES ${CMAKE_CURRENT_SOURCE_DIR}/*.l)
+	file(GLOB_RECURSE MY_BISON_FILES ${CMAKE_CURRENT_SOURCE_DIR}/*.y)
+	# remove extension from .l and .y files
+	REMOVE_FILE_EXTENSION(${MY_FLEX_FILES} flexFiles)
+	REMOVE_FILE_EXTENSION(${MY_BISON_FILES} bisonFiles)
+endif()
 	
 #------ DEPRECATRED RCC++ Include dirs -----
 FOREACH(currentDir ${CURRENT_INCLUDE_DIRS})
@@ -289,7 +298,8 @@ ENDIF(MSVC)
 endif()
 
 
-
+# Force C++
+set_source_files_properties(${MY_SRC} PROPERTIES LANGUAGE CXX)
 
 #----- CREATE TARGET -----
 if(${mode} STREQUAL "STATIC")
@@ -350,38 +360,59 @@ ENDIF(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/some_file)
 
 
 #----- Flex and Bison -----
-SET( FLEX_EXECUTABLE "${CMAKE_SOURCE_DIR}/CMake/flex_bison/flex.exe")
-SET( BISON_EXECUTABLE "${CMAKE_SOURCE_DIR}/CMake/flex_bison/bison.exe")
+if( USE_FLEX_AND_BISON AND NOT FLEX_EXECUTABLE STREQUAL "" AND NOT BISON_EXECUTABLE STREQUAL "")
+SET( FLEX_EXECUTABLE1 "${CMAKE_SOURCE_DIR}/3rdParty/FlexBison/bin/flex.exe")
+SET( BISON_EXECUTABLE1 "${CMAKE_SOURCE_DIR}/3rdParty/FlexBison/bin/bison.exe")
+	SET( FlexArgs "-o${MY_FLEX_FILES}.flex.c" "${MY_FLEX_FILES}.l")
+	SET( BisonArgs "-o${MY_BISON_FILES}.bison.c" "-d" "${MY_BISON_FILES}.y" )
+	
+	#[[
+	# Create target for the parser
+	ADD_CUSTOM_TARGET(${PROJECT_NAME}Flex ALL
+		COMMAND ${FLEX_EXECUTABLE} 
+		-o"${flexFiles}.flex.c"
+		"${flexFiles}.l"
+		COMMENT "Creating .flex.c")
+		
+	ADD_CUSTOM_TARGET(${PROJECT_NAME}Bison ALL
+		COMMAND ${BISON_EXECUTABLE}
+		-o"${bisonFiles}.bison.c"
+		-d "${bisonFiles}.y"
+		COMMENT "Creating .bison.c")
+	add_dependencies(${PROJECT_NAME} ${PROJECT_NAME}Flex)
+	add_dependencies(${PROJECT_NAME}Flex ${PROJECT_NAME}Bison)
+	 
+	if( MSVC )
+		SET_PROPERTY(GLOBAL PROPERTY USE_FOLDERS ON)
+		SET_PROPERTY(TARGET ${PROJECT_NAME}Flex		PROPERTY FOLDER CMakePredefinedTargets)
+		SET_PROPERTY(TARGET ${PROJECT_NAME}Bison		PROPERTY FOLDER CMakePredefinedTargets)
+	endif()
+	]]#
+	
+	
+	ADD_CUSTOM_TARGET(${PROJECT_NAME}FlexBison echo "Generating Flex & Bison source files")
+		
+	ADD_CUSTOM_COMMAND(
+	   DEPENDS ${flexFiles}.l
+	   COMMAND ${FLEX_EXECUTABLE}
+	   ARGS -o"${flexFiles}.flex.c"
+			"${flexFiles}.l"
+	   TARGET ${PROJECT_NAME}FlexBison
+	   OUTPUTS ${flexFiles}.flex.c)
+	   
+	ADD_CUSTOM_COMMAND(
+	   DEPENDS ${flexFiles}.y
+	   COMMAND ${BISON_EXECUTABLE}
+	   ARGS -o"${bisonFiles}.bison.c"
+			"${bisonFiles}.y"
+	   TARGET ${PROJECT_NAME}FlexBison
+	   OUTPUTS ${bisonFiles}.bison.c)
 
-SET( FlexArgs "-o${MY_FLEX_FILES}.flex.c" "${MY_FLEX_FILES}.l")
-SET( BisonArgs "-o${MY_BISON_FILES}.bison.c" "-d" "${MY_BISON_FILES}.y" )
-
-#execute_process(COMMAND ${FLEX_EXECUTABLE} ${FlexArgs})
-#execute_process(COMMAND ${BISON_EXECUTABLE} ${BisonArgs})
-
-#message("${FLEX_EXECUTABLE}")
-#message("${BISON_EXECUTABLE}")
-#MESSAGE("${${PROJECT_NAME}_SOURCE_DIR}/src/lexer.l")
-# Create target for the parser
-ADD_CUSTOM_TARGET(${PROJECT_NAME}Flex ALL
-	COMMAND ${FLEX_EXECUTABLE} 
-	-o"${flexFiles}.flex.c"
-	"${flexFiles}.l"
-	COMMENT "Creating .flex.c")
-
-ADD_CUSTOM_TARGET(${PROJECT_NAME}Bison ALL
-	COMMAND ${BISON_EXECUTABLE}
-	-o"${bisonFiles}.bison.c"
-	-d "${bisonFiles}.y"
-	COMMENT "Creating .bison.c")
-
-add_dependencies(${PROJECT_NAME} ${PROJECT_NAME}Flex)
-add_dependencies(${PROJECT_NAME}Flex ${PROJECT_NAME}Bison)
- 
-if( MSVC )
-	SET_PROPERTY(GLOBAL PROPERTY USE_FOLDERS ON)
-	SET_PROPERTY(TARGET ${PROJECT_NAME}Flex		PROPERTY FOLDER CMakePredefinedTargets)
-	SET_PROPERTY(TARGET ${PROJECT_NAME}Bison		PROPERTY FOLDER CMakePredefinedTargets)
+	add_dependencies(${PROJECT_NAME} ${PROJECT_NAME}FlexBison)
+		if( MSVC )
+		SET_PROPERTY(GLOBAL PROPERTY USE_FOLDERS ON)
+		SET_PROPERTY(TARGET ${PROJECT_NAME}FlexBison		PROPERTY FOLDER CMakePredefinedTargets)
+		endif()
 endif()
 
 
